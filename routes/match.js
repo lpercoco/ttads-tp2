c35c734e4cb95ccb5450fd63a21697be134d5ae1
rdjah <lisandro.percoco@hotmail.com>
2017-12-06 17:40:28 -0300

    api que devuelve partidos activos funcionando

--- routes/match.js
+++ routes/match.js
@@ -7,26 +7,57 @@ var Match = mongoose.model('match');
 
 //mock data
 
-// var matchOne = new Match({
-// id: 1,
-// localTeam: {type: mongoose.Schema.Types.ObjectId, ref: 'team', required: true},
-// visitorTeam: {type: mongoose.Schema.Types.ObjectId, ref: 'team', required: true},
-// events:[{type: mongoose.Schema.Types.ObjectId, ref: 'matchEvent'}],
-// beginOfMatch: {type: Date},
-// endOfMatch: {type: Date},
-// matchOne.save();
+Match.remove().exec();
 
+//var dateOne=new Date(1992, 1, 1, 19, 30);
+
+var matchOne = new Match({
+  id: 1,
+  localTeam: '5a2082c711a43f72d2da7714',
+  visitorTeam: '5a2082c711a43f72d2da7715',
+  beginOfMatch: new Date()
+});
+
+matchOne.save();
+
+var matchTwo = new Match({
+  id: 2,
+  localTeam: '5a2082c711a43f72d2da7715',
+  visitorTeam: '5a2082c711a43f72d2da7714',
+  beginOfMatch: new Date(),
+  endOfMatch: new Date(1992, 1, 1, 19, 30)
+});
+
+matchTwo.save();
 
 //
 
 //get all matchs
 router.get('/', (req, res, next) => {
   Match.find({})
+  .populate('localTeam')
+  .populate('visitorTeam')
+  .populate('events')
   .then(matchs =>{
-    if(!matchs.length){ return res.sendStatus(404); }
-    return res.json({'matchs': matchs})
+    if(!matchs.length){ return res.sendStatus(204); }
+    else{return res.json({'matchs': matchs});}
   })
   .catch(next);
 });
+
+//get not finished matchs
+router.get('/active', (req, res, next) => {
+  Match.find({endOfMatch: null})
+  .populate('localTeam')
+  .populate('visitorTeam')
+  .populate('events')
+  .then(matchs =>{
+    if(!matchs.length){ return res.sendStatus(204); }
+    else{return res.json({'matchs': matchs});}
+  })
+  .catch(next);
+});
+
+
 
 module.exports=router;
